dc3384628330ebb6fb2040e677001f1033744619
g4eml <colin@durbridge.me.uk>
2020-05-17 18:29:37 +0100

    Kill udpts.sh before updating and restarting
    Wait for inbuilt udpts.sh to start then kill it off
    Copy updated version of files
    Restart new version of udpts.sh

--- runme0.sh
+++ runme0.sh
@@ -1,7 +1,25 @@
+# wait for inbuilt udpts.sh to start
+
+while !(ps -a | grep -v grep | grep -q udpts.sh); do
+sleep 1
+done
+
+#stop it
+
+killall udpts.sh
+
+#copy the updated versions 
+
 cp -f /media/sda/encoder_control.php /www
 cp -f /media/sda/pluto.php /www
 cp -f /media/sda/save.php /www
 cp -f /media/sda/udpts.sh /root
 cp -f /media/sda/strategy.sh /root
 
+# create a dummy file to confirm this script has been run.
 
+touch /root/Patches_Loaded!
+
+#start the new version of udpts.sh
+
+/root/udpts.sh > /dev/null < /dev/null 2> /dev/null &
